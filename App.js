e85e9189abdf9c3f47d0b2c6a14266112a69257b
smiadv2022 <msorokina@spacemedia.com.ua>
2023-06-22 23:20:04 +0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>!React-Native-hw1</Text>
+      <Text>!!!!!!!!!React-Native-hw1</Text>
       <StatusBar style="auto" />
     </View>
   );
